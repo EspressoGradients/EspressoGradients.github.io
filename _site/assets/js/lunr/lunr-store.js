ae6fd74dbcca17508bf0a21d2c877ded3683d452
Nina Elmer <ninaluebbe1@gmail.com>
2025-09-27 20:31:44 +0200

    initial commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Mein Erster Eintrag",
+        "excerpt":"Dies ist ein erster Testeintrag um meine Reise von Heidelberg nach Cambridge zu dokumentieren.   You can write Markdown as usual:      Lists   Code blocks   Images  ","categories": ["blog"],
+        "tags": ["welcome","jekyll"],
+        "url": "/blog/welcome/",
+        "teaser": null
+      }]
