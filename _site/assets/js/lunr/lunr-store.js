5d58cf807ada9ce69acb4a5b040826ae2df2873f
Nina Elmer <nina.elmer99@gmail.com>
2025-10-08 12:42:35 +0100

    bug fixes

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Mein Erster Eintrag",
-        "excerpt":"Dies ist ein erster Testeintrag um meine Reise von Heidelberg nach Cambridge zu dokumentieren.   You can write Markdown as usual:      Lists   Code blocks   Images  ","categories": ["blog"],
-        "tags": ["welcome","jekyll"],
-        "url": "/blog/welcome/",
+        "title": "Goodbye Deutschland - Neuanfang",
+        "excerpt":"Abschiede und Neuanfänge — zwei Seiten derselben Medaille   Letzten Sonntag war es dann so weit, nachdem ich erst von Heidelberg nach OWL gezogen bin für eine Woche, hieß es auch da Abschied nehmen und nach Hannover zum Flughafen zu fahren, um von dort aus nach London zu fliegen. Abschiede...","categories": ["blog"],
+        "tags": ["life"],
+        "url": "/blog/goodbye/",
         "teaser": null
       }]
